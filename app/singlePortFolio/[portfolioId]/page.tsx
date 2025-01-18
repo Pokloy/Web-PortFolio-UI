40e916695db824239a85503f43c331c86e766956
AlierT <tetsu.kala@gmail.com>
2025-01-18 16:55:16 +0800

    pushing on the correction eeror 1

--- app/singlePortFolio/[portfolioId]/page.tsx
+++ app/singlePortFolio/[portfolioId]/page.tsx
@@ -6,11 +6,7 @@ interface Params {
   portfolioId: string;
 }
 
-interface PageProps {
-  params: Params;
-}
-
-const Page = ({ params }: PageProps) => {
+const Page = ({ params }: { params: Params }) => {
   const portfolioId = params.portfolioId;
 
   if (!portfolioId) {
@@ -25,16 +21,6 @@ const Page = ({ params }: PageProps) => {
 }
 
 export default Page;
-
-export async function getServerSideProps(context: any) {
-  const { portfolioId } = context.params;
-  return {
-    props: {
-      params: { portfolioId }, // passing `params` to the component
-    },
-  };
-}
-
 
 
 //client version

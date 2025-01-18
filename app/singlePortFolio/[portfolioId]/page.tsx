bea824e505ef3fc49245b9e1b95fbd35b4a37064
AlierT <tetsu.kala@gmail.com>
2025-01-18 17:05:01 +0800

    pushing on the error 2

--- app/singlePortFolio/[portfolioId]/page.tsx
+++ app/singlePortFolio/[portfolioId]/page.tsx
@@ -1,46 +1,46 @@
 // server version
-import React from 'react'
-import SinglePortFolio from '../singlePortFolio';
-
-interface Params {
-  portfolioId: string;
-}
-
-const Page = ({ params }: { params: Params }) => {
-  const portfolioId = params.portfolioId;
-
-  if (!portfolioId) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <main className='min-h-screen flex flex-col'>
-      <SinglePortFolio portfolioId={portfolioId} />
-    </main>
-  )
-}
-
-export default Page;
-
-
-//client version
-// "use client"
-
 // import React from 'react'
 // import SinglePortFolio from '../singlePortFolio';
-// import { useParams } from "next/navigation";
 
-// const page: React.FC = () => {
-//   const params = useParams();
-//   const portfolioId = params?.portfolioId;
+// interface Params {
+//   portfolioId: string;
+// }
 
-//   const portFolioIdString = typeof portfolioId === "string" ? portfolioId : "";
+// const Page = ({ params }: { params: Params }) => {
+//   const portfolioId = params.portfolioId;
+
+//   if (!portfolioId) {
+//     return <div>Loading...</div>;
+//   }
 
 //   return (
-//     <main>
-//       <SinglePortFolio portfolioId={portFolioIdString} />
+//     <main className='min-h-screen flex flex-col'>
+//       <SinglePortFolio portfolioId={portfolioId} />
 //     </main>
 //   )
 // }
 
-// export default page;
+// export default Page;
+
+
+//client version
+"use client"
+
+import React from 'react'
+import SinglePortFolio from '../singlePortFolio';
+import { useParams } from "next/navigation";
+
+const page: React.FC = () => {
+  const params = useParams();
+  const portfolioId = params?.portfolioId;
+
+  const portFolioIdString = typeof portfolioId === "string" ? portfolioId : "";
+
+  return (
+    <main>
+      <SinglePortFolio portfolioId={portFolioIdString} />
+    </main>
+  )
+}
+
+export default page;
